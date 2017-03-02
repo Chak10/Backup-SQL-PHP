d87838f7ec1c3382e488ffc61f0d5b9c3c619643
Chak10 <francescosorb@live.it>
2017-03-02 16:20:27 +0100

    EXAMPLE 2

--- use/example.php
+++ use/example.php
@@ -12,10 +12,15 @@
 	$test1 = new SQL_Backup();
 	
 	$test1->con(DB_HOST,DB_USER,DB_PASSWD,DB_NAME);
+	
 	$test1->table_name='alldata'; 
+	
 	$test1->ext = $test1::SQL; 
+	
 	$test1->compress = true;
+	
 	$test1->alltable_in_file = true;
+	
 	$test1->save = false;
 	
 	$test1->execute();
