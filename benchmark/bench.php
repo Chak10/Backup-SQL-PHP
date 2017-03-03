c94c0f22c3197c9a9bf3e05e5db5aad9e17d8001
Chak10 <francescosorb@live.it>
2017-03-04 00:37:09 +0100

    clean

--- benchmark/bench.php
+++ benchmark/bench.php
@@ -6,30 +6,135 @@
 	DEFINE ('DB_HOST', 'localhost');
 	DEFINE ('DB_NAME', 'francesco');
 	DEFINE ('DB_PASSWD', '');
-	$table_name = "alldata";
-	$bench = array();
-	$i=0;
-	while($i <25){
+	
+	$con = new mysqli(DB_HOST,DB_USER,DB_PASSWD,DB_NAME);
+	
+	$final = array();	
+	$type = 13;
+	$n = 10;
+	$fin =0;
+	for($x=0;$x<$n;++$x){
 		$time = -microtime(true);
-		
-		$con = new mysqli(DB_HOST,DB_USER,DB_PASSWD,DB_NAME);
-		$test1 = new SQL_Backup($con,$table_name);
-		$test1->ext=$test1::SQL;
-		$test1->json_pretty=true;
-		$test1->save = false;
-		$test1->info_t = true;
+		$test1 = new SQL_Backup($con,'alldata');
+		$test1->ext=$type;
 		$test1->execute();
-		
 		$time += microtime(true);
-		
-		$bench["Time_".$i] = $time;
-		++$i;
+		$fin += $time;
 	}
-	$bench =array_sum($bench);
-	$fin = array("Info"=>$test1->info,"Tot"=>$bench,"Avg"=>$bench/25);
+	$final["SQL_NOCOMPRESSED_X10"]=$fin/$n;
 	
-	$test2 = file_put_contents("res/test_nosave_SQL_X25_".time().'.json',json_encode($fin,JSON_PRETTY_PRINT));
+	$type = 26;
+	$n = 10;
+	$fin =0;
+	for($x=0;$x<$n;++$x){
+		$time = -microtime(true);
+		$test1 = new SQL_Backup($con,'alldata');
+		$test1->ext=$type;
+		$test1->execute();
+		$time += microtime(true);
+		$fin += $time;
+	}
+	$final["CSV_NOCOMPRESSED_X10"]=$fin/$n;
 	
-	var_dump ($fin);
+	$type = 49;
+	$n = 10;
+	$fin =0;
+	for($x=0;$x<$n;++$x){
+		$time = -microtime(true);
+		$test1 = new SQL_Backup($con,'alldata');
+		$test1->ext=$type;;
+		$test1->alltable_in_file=true;
+		$test1->execute();
+		$time += microtime(true);
+		$fin += $time;
+	}
+	$final["JSON_NOCOMPRESSED_X10"]=$fin/$n;
+	$type = 13;
+	$n = 10;
+	$fin =0;
+	for($x=0;$x<$n;++$x){
+		$time = -microtime(true);
+		$test1 = new SQL_Backup($con,'alldata');
+		$test1->ext=$type;
+		$test1->alltable_in_file=true;
+		$test1->compress=true;
+		$test1->execute();
+		$time += microtime(true);
+		$fin += $time;
+	}
+	$final["SQL_COMPRESSED_X10(No-separate)"]=$fin/$n;
+	
+	$type = 26;
+	$n = 10;
+	$fin =0;
+	for($x=0;$x<$n;++$x){
+		$time = -microtime(true);
+		$test1 = new SQL_Backup($con,'alldata');
+		$test1->ext=$type;
+		$test1->alltable_in_file=true;
+		$test1->compress=true;
+		$test1->execute();
+		$time += microtime(true);
+		$fin += $time;
+	}
+	$final["CSV_COMPRESSED_X10(No-separate)"]=$fin/$n;
+	
+	$type = 49;
+	$n = 10;
+	$fin =0;
+	for($x=0;$x<$n;++$x){
+		$time = -microtime(true);
+		$test1 = new SQL_Backup($con,'alldata');
+		$test1->ext=$type;
+		$test1->alltable_in_file=true;
+		$test1->compress=true;
+		$test1->execute();
+		$time += microtime(true);
+		$fin += $time;
+	}
+	$final["JSON_COMPRESSED_X10(No-separate)"]=$fin/$n;
+	$type = 13;
+	$n = 10;
+	$fin =0;
+	for($x=0;$x<$n;++$x){
+		$time = -microtime(true);
+		$test1 = new SQL_Backup($con,'alldata');
+		$test1->ext=$type;
+		$test1->compress=true;
+		$test1->execute();
+		$time += microtime(true);
+		$fin += $time;
+	}
+	$final["SQL_COMPRESSED_X10(Separate)"]=$fin/$n;
+	
+	$type = 26;
+	$n = 10;
+	$fin =0;
+	for($x=0;$x<$n;++$x){
+		$time = -microtime(true);
+		$test1 = new SQL_Backup($con,'alldata');
+		$test1->ext=$type;
+		$test1->compress=true;
+		$test1->execute();
+		$time += microtime(true);
+		$fin += $time;
+	}
+	$final["CSV_COMPRESSED_X10(Separate)"]=$fin/$n;
+	
+	$type = 49;
+	$n = 10;
+	$fin =0;
+	for($x=0;$x<$n;++$x){
+		$time = -microtime(true);
+		$test1 = new SQL_Backup($con,'alldata');
+		$test1->ext=$type;
+		$test1->compress=true;
+		$test1->execute();
+		$time += microtime(true);
+		$fin += $time;
+	}
+	$final["JSON_COMPRESSED_X10(Separate)"]=$fin/$n;
+	
+	var_dump(json_encode($final,JSON_PRETTY_PRINT));
 	
 ?>
