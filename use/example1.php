f3b8fe136223ccfc0504c840aa87e151761d4477
Chak10 <francescosorb@live.it>
2017-03-05 13:18:02 +0100

    Update example1.php

--- use/example1.php
+++ use/example1.php
@@ -10,7 +10,7 @@
 	$test1 = new SQL_Backup();	
 	$test1->con(DB_HOST,DB_USER,DB_PASSWD,DB_NAME);	
 	$test1->table_name='alldata';
- 	$test1->folder = $folder;
+ 	$test1->folder = 'folder';
 	$test1->ext = $test1::SQL; 	
 	$test1->compress = true;
 	$test1->alltable_in_file = true;	
