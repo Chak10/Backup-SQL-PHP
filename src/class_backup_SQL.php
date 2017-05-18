912b3e1ebe63a8815656e401e95a77ca0fc2c40c
Chak10 <francescosorb@live.it>
2017-05-18 23:40:45 +0200

    Update class_backup_SQL.php

--- src/class_backup_SQL.php
+++ src/class_backup_SQL.php
@@ -1,17 +1,6 @@
 <?php
 class SQL_Backup {
     
-    /*
-    
-    GNU GENERAL PUBLIC LICENSE
-    Version 3, 29 June 2007
-    
-    Copyright (C) 2007 Free Software Foundation, Inc. <http://fsf.org/>
-    Everyone is permitted to copy and distribute verbatim copies
-    of this license document, but changing it is not allowed.
-    
-    */
-    
     private $version = "1.0.7";
     private $site = "https://github.com/Chak10/Backup-SQL-By-Chak10.git";
     
